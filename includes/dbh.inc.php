8bbded6dbcd0cfd9d752da8ae97440532a3ad64b
Abdellah <hibazekraoui23@email.com>
2026-04-24 10:19:03 +0100

    finished the project

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -7,5 +7,4 @@ function ConnectionDb(){
         echo $e->getMessage();
     }
 }
-
 ?>
--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -1,5 +1,4 @@
 <?php
-
 if(isset($_POST["singup"])){
     require "../includes/dbh.inc.php";
 $conn=ConnectionDb();
